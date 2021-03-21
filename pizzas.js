19ed02088dd3cd3c6c7bd5408ef8e359c7bee7da
Adriano Ivan <adrianoivandepaulacampos@gmail.com.br>
2021-03-21 09:00:51 -0300

    Arquivo com o array de objetos que representam os dados das pizzas

---
+++ pizzas.js
@@ -0,0 +1,15 @@
+let pizzaJson = [
+    {id:1, name:"Mussarela", img:"img/pizza.png", price:20.19, sizes:["100g","530g","860g"],
+    description:"Deliciosa pizza de mussarela com a nossa única identidade !"},
+    {id:2, name:"Calabresa", img:"img/pizza2.png", price:18.00, sizes:["320g","530g","860g"],
+    description:"Pizza da calabresa mais cuidadosamente selecionada"},
+    {id:3, name:"Quatro Queijos", img:"img/pizza3.png", price:17.45, sizes:["320g","530g","860g"],description:"O queijo mais delicioso que você irá experimentar !"},
+    {id:4, name:"Americana", img:"img/pizza4.png", price:19.77, sizes:["320g","530g","860g"],
+    description:"A única autêntica !"},
+    {id:5, name:"Sorvete", img:"img/pizza5.png", price:21.43, sizes:["320g","530g","860g"],
+    description:"Um casamento perfeito !"},
+    {id:6, name:"Moda da casa", img:"img/pizza6.png", price:18.55, sizes:["320g","530g","860g"],
+    description:"A mais famosa e experimentada !"},
+    {id:7, name:"Chocolate", img:"img/pizza7.png", price:22.36, sizes:["320g","530g","860g"],
+    description:"O chocolate é caseiro e da mais caprichada têcnica, hem !"}
+]
